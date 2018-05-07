66afaa0bb152709059bd910c1a4892388b635d8f
Hannah-Rogers <hannahrogers@Hannahs-MacBook-Pro.local>
2018-05-07 17:07:56 -0600

    intial commit

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 "use-strict";
 
+var loadPage = (function(page) {
 
-var myPage = (function(page) {
-
-/* variables -------------------------------*/
-    var usda_api = {
+/* variables-------------------------------*/
+const usda_api = {
       key: "krBcPpdX6JTkBNyzXjJU8a65fzYQxOEZbFOzxHQT",
       searchURL: "https://api.nal.usda.gov/ndb/search/?format=json",
       nutrientURL: "https://api.nal.usda.gov/ndb/nutrients/?format=json"
     };
 
-    var nutrients = {
+const nutrients = {
       protein: "203",
       fat: "204",
       carbs: "205",
@@ -21,7 +20,7 @@ var myPage = (function(page) {
 
 
 /* url functions-------------------------------*/
-    var _getSearchURL = function(query) {
+function getSearchURL(query) {
       var URL = usda_api.searchURL;
       URL += "&api_key=" + usda_api.key;
       URL += "&q=" + query;
@@ -30,7 +29,7 @@ var myPage = (function(page) {
     }
 
 
-    var _getNutrientURL = function(id) {
+function getNutrientURL(id) {
       var URL = usda_api.nutrientURL;
       URL += "&api_key=" + usda_api.key;
       URL += "&nutrients=" + nutrients.protein;
@@ -45,12 +44,9 @@ var myPage = (function(page) {
     }
 
 
-    /* html functions
-    -------------------------------*/
-    var _getResultsHTML = function(data) {
+/* html functions-------------------------------*/
+function getResultsHTML(data) {
       var HTML = "";
-      
-      // loop through data and build HTML
       $.each(data["list"]["item"], function (i, obj) {
         var foodName, foodID;
         $.each(obj, function (key, value) {
@@ -62,30 +58,23 @@ var myPage = (function(page) {
               foodID = value;
               break;
           }
-        }); // end of inner loop
+        }); 
 
-
-
-        // concat html string
         HTML += "<div class='searchItem' data-ndbno='" + foodID + "' data-name='" + foodName + "'>";
         HTML += foodName;
         HTML += "</div>"
 
-      }); // end of outer loop
-
+      }); 
       return HTML;
     }
 
 
-    var _getNutrientHTML = function(data) {
-      // format the header
+function getNutrientHTML(data) {
       var header = data["report"]["foods"][0]["name"];
       
-      // append header and severing size
       $(".js-nutrient-header").html(header);
       $(".js-nutrient-serving").html("Serving Size: " + data["report"]["foods"][0]["measure"]);
 
-      // loop through data and build html
       var HTML = "";
       $.each(data["report"]["foods"][0]["nutrients"], function (i, obj) {
         var nutrientName, nutrientValue;
@@ -128,109 +117,79 @@ var myPage = (function(page) {
               }
               break;
 
-          } // end of switch
-        }); // end of inner loop
-
-        // concat html string
+          } 
+        }); 
         HTML += "<div class='nutrients' >" + nutrientName + ": " + nutrientValue + "</div>";
-      });   // end of outer loop
-
+      });   
       return HTML;
     }
 
 
-    var _getErrorHTML = function() {
+function getErrorHTML() {
       return "<div style='text-align:center;'>No results found</div>"
     }
 
 
-    /* ajax calls
-    -------------------------------*/
-    var search = function(query) {
+/* ajax calls-------------------------------*/
+function getSearchData(query) {
       $.ajax({
         type: "GET",
-        url: _getSearchURL(query),
+        url: getSearchURL(query),
         success: function (data) {
-          $(".js-search-results").html(_getResultsHTML(data));
+          $(".js-search-results").html(getResultsHTML(data));
         },
         error: function (xhr, error) {
-          //console.debug(xhr); console.debug(error);
-          $(".js-search-results").html(_getErrorHTML());
+          $(".js-search-results").html(getErrorHTML());
         }
       });
     }
     
 
-    var getNutrients = function(id) {
+function getNutrientData(id) {
       $.ajax({
         type: "GET",
-        url: _getNutrientURL(id),
+        url: getNutrientURL(id),
         success: function (data) {
           if (data["report"]["foods"].length > 0) {
-            // build results html
-            $(".js-nutrient-list").html(_getNutrientHTML(data));
+            $(".js-nutrient-list").html(getNutrientHTML(data));
           }
           else {
-            // show no results
-            $(".js-nutrient-list").html(_getErrorHTML());
+            $(".js-nutrient-list").html(getErrorHTML());
           }
-
- 
         },
         error: function (xhr, error) {
-          //console.debug(xhr); console.debug(error);v
-          
-          // open the nutrient modal with error message
-          $(".js-nutrient-list").html(_getErrorHTML());
+          $(".js-nutrient-list").html(getErrorHTML());
         }
       });
     }
 
 
-    /* create usda namespace
-    -------------------------------*/
+/* create usda namespace-------------------------------*/
     page.usda = {
-      search: search,
-      getNutrients: getNutrients
+      getSearchData: getSearchData,
+      getNutrientData: getNutrientData
     };
-
     return page;
+  })(loadPage || {});
 
-  })(myPage || {});
 
-
-/*=================================
-  page_events.js
-  =================================*/
+/*event listeners---------------------------------*/
   !function(page) {
-    "use strict";
-
-    /* txtSearch
-    -------------------------------*/
     $(".js-query").on("keypress", function(e) {
-      // pressed enter key
       if (e.keyCode === 13) {
         e.preventDefault();
-
-        // search usda database
         var query = $(".js-query").val();
-        page.usda.search(query);
-
-        // remove focus
+        page.usda.getSearchData(query);
         $(".js-query").blur();
       }
     });
 
-
-    /* results
-    -------------------------------*/
     $(".js-search-results")
-      // fetch nutrient data
       .on("click", ".searchItem", function() {
         $('.show-nutrients').removeClass('hidden');
         var id = $(this).attr('data-ndbno');
-        page.usda.getNutrients(id);
+        page.usda.getNutrientData(id);
       });
 
-  }(myPage);
+  }(loadPage);
 
